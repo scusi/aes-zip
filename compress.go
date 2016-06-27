04d33c15848ed3f5f69d679079178d41dc1d0b04
Scusi <scusi@snurn.de>
2016-06-27 18:58:33 +0200

    cleanup

--- compress.go
+++ compress.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"strings"
     "flag"
+    "log"
+    "fmt"
 )
 
 var zipFile string
@@ -14,23 +16,37 @@ var passwd string
 var target string
 var needBaseDir bool
 
+var Usage = func() {
+    // usage: compress [-p password] [--includeBaseDir=true] zipfile file1 [file2 dir1 dir2 file3 [...]]
+    fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+    fmt.Fprintf(os.Stderr, "%s [-p password] [--includeBaseDir=false] zipfile file [otherFile dir otherdir [...]]\n", os.Args[0])
+    flag.PrintDefaults()
+    fmt.Fprintln(os.Stderr, "")
+    fmt.Fprintf(os.Stderr, "%s is written by scusi - https://github.com/scusi/")
+}
 func init() {
 	flag.StringVar(&zipFile, "f", "", "zip file to create")
 	flag.StringVar(&passwd, "p", "infected", "password to use")
-    flag.StringVar(&target, "d", "./", "file/dir to add to zip")
-    flag.BoolVar(&needBaseDir, "includeBaseDir", true, "per default includes the dir structure")
+    //flag.StringVar(&target, "d", "./", "file/dir to add to zip")
+    flag.BoolVar(&needBaseDir, "includeBaseDir", true, "per default a base directory is included nto zip archive")
 }
 
 func main() {
     flag.Parse()
-    //err := zipit(target, zipFile, false)
-    err := zipit(target, zipFile, needBaseDir)
+
+    filesToCompress := flag.Args()
+    if len(filesToCompress) < 1 {
+        log.Fatal("no file(s) to compress given. First argument - and following - must be files to add to archive")
+    }
+
+    err := zipit(zipFile, needBaseDir, filesToCompress)
     if err != nil {
         panic(err)
     }
 }
 
-func zipit(source, target string, needBaseDir bool) error {
+//func zipit(source, target string, needBaseDir bool) error {
+func zipit(target string, needBaseDir bool, sources []string) error {
 	zipfile, err := os.Create(target)
 	if err != nil {
 		return err
@@ -39,8 +55,8 @@ func zipit(source, target string, needBaseDir bool) error {
 
 	archive := zip.NewWriter(zipfile)
 	defer archive.Close()
-    //archive.SetPassword(passwd)
 
+    for _, source := range sources {
 	info, err := os.Stat(source)
 	if err != nil {
 		return err
@@ -98,5 +114,6 @@ func zipit(source, target string, needBaseDir bool) error {
 		return err
 	})
 
+    }
 	return err
 }
